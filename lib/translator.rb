39381f0dce54a1df45a0b8419a9ad6250fd991e6
Michael Schneider <mic.sch91@gmail.com>
2020-06-08 15:21:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,15 @@
 require 'pry'
 require "yaml"
 # require modules here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  new_hash = {}
+
+  emoticons.each do |key,value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+
+  new_hash
+end
