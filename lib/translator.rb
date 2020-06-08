9ab5d92363cabe56b42ec34913b338cfdf03c44c
Michael Schneider <mic.sch91@gmail.com>
2020-06-08 15:22:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,3 +13,11 @@ def load_library(file_path)
 
   new_hash
 end
+
+def get_japanese_emoticon(file_path, emoticon)
+  library = load_library(file_path)
+  emoticon = library.keys.find do |key|
+    library[key][:english] == emoticon
+  end
+  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
+end
